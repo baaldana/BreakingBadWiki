8b87a5c46e493e2898379e30125d361e0ece8f3c
Benjamin Aldana <baaldana@uc.cl>
2021-04-04 11:03:52 -0400

    Added react project

--- brbawiki/brbawiki/settings.py
+++ brbawiki/brbawiki/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(&h*&5@=o!-_&k5%v+89lw%r-7h(p(ro=p$w&#g99650g9rv!q'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
