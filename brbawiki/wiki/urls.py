9fa7af62cef5e46ebe3b4b79423113d1cee42066
Benjamin Aldana <baaldana@uc.cl>
2021-04-06 19:03:52 -0400

    Seasons, episodes by season, and specific episodes available

--- brbawiki/wiki/urls.py
+++ brbawiki/wiki/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('episodes/', views.episodes, name='episodes'),
     path('characters/<int:question_id>/', views.character, name='character'),
     path('characters/', views.characters, name='characters'),
+    path('brba-seasons/<int:question_id>/', views.brba_seasons, name='brba_seasons'),
+    path('bcs-seasons/<int:question_id>/', views.bcs_seasons, name='bcs_seasons'),
     # path('episodes/<int:question_id>/', views.episode, name=''),
     # path('episodes/', views.episodes, name='episodes'),
 ]
--- brbawiki/wiki/views.py
+++ brbawiki/wiki/views.py
@@ -8,24 +8,22 @@ import json
 
 api_url = 'https://tarea-1-breaking-bad.herokuapp.com/api/'
 
+
 def index(request):
-    url = f'{api_url}episodes/'
-    response = requests.get(url).json()
-    brba_episodes = []
+    url_brba = f'{api_url}episodes?series=Breaking+Bad'
+    url_bcs = f'{api_url}episodes?series=Better+Call+Saul'
+    brba_episodes = requests.get(url_brba).json()
     brba_seasons = []
-    bcs_episodes = []
+    bcs_episodes = requests.get(url_bcs).json()
     bcs_seasons = []
 
 
-    for episode in response:
-        if episode['series'] == "Breaking Bad":
-            brba_episodes.append(episode)
-            if episode['season'] not in brba_seasons:
-                brba_seasons.append(episode['season'])
-        else:
-            bcs_episodes.append(episode)
-            if episode['season'] not in bcs_seasons:
-                bcs_seasons.append(episode['season'])
+    for episode in brba_episodes:
+        if episode['season'] not in brba_seasons:
+            brba_seasons.append(episode['season'])
+    for episode in bcs_episodes:
+        if episode['season'] not in bcs_seasons:
+            bcs_seasons.append(episode['season'])
     context = {
         "brba_episodes": brba_episodes,
         "brba_seasons": brba_seasons,
@@ -34,10 +32,52 @@ def index(request):
         }
     return render(request, 'wiki/index.html', context)
 
+def brba_seasons(request, question_id):
+    url = f'{api_url}episodes?series=Breaking+Bad'
+    brba_episodes = requests.get(url).json()
+    season_episodes = []
+    for episode in brba_episodes:
+        if episode['season'] == str(question_id):
+            season_episodes.append(episode)
+    
+    context = {
+        "season_episodes": season_episodes,
+        "series": "Breaking Bad",
+        "season": question_id
+        }
+    return render(request, 'wiki/season.html', context)
+
+def bcs_seasons(request, question_id):
+    url = f'{api_url}episodes?series=Better+Call+Saul'
+    bcs_episodes = requests.get(url).json()
+    season_episodes = []
+    for episode in bcs_episodes:
+
+        if episode['season'] == str(question_id):
+            season_episodes.append(episode)
+
+    context = {
+        "season_episodes": season_episodes,
+        "series": "Better Call Saul",
+        "season": question_id
+        }
+    return render(request, 'wiki/season.html', context)
+
+
 def episode(request, question_id):
-    url = f'{api_url}episodes/{question_id}/'
-    response = requests.get(url).json()
-    context = {"response": response}
+    url_episode = f'{api_url}episodes/{question_id}/'
+    episode = requests.get(url_episode).json()[0]
+    
+    episode['air_date'] = episode['air_date'][8:10] + "-"+ episode['air_date'][5:7] + "-" +episode['air_date'][:4] 
+    
+    character_names = episode['characters']
+    for index in range(len(character_names)):
+        url_character = f'{api_url}characters?name={character_names[index].replace(" ","+")}' 
+        character_id = requests.get(url_character).json()[0]['char_id']
+        episode['characters'][index] = [character_names[index], character_id]
+
+
+    context = {"episode": episode}
     return render(request, 'wiki/episode.html', context)
 
 def episodes(request):
