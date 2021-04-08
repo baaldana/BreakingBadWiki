28c668c6daea8c12a79704736a9cb82ea2fb778d
Benjamin Aldana <baaldana@uc.cl>
2021-04-08 18:55:18 -0400

    removed herokuapp from installed apps at settings

--- brbawiki/settings.py
+++ brbawiki/settings.py
@@ -46,8 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'herokuapp',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
